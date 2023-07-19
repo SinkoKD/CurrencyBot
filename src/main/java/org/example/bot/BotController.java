4deef9ce9e7d120d626db7c07ab29f39ac41c553
kostiantyn <sinkkostia@gmail.com>
2023-07-19 19:54:18 +0300

    new one

--- src/main/java/org/example/bot/BotController.java
+++ src/main/java/org/example/bot/BotController.java
@@ -40,8 +40,6 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
 import static com.pengrad.telegrambot.model.request.ParseMode.HTML;
-import static org.example.bot.JedisDB.*;
-
 
 
 public class BotController {
@@ -324,5 +322,73 @@ public class BotController {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             jedisPool.close();
         }));
+    }
+
+    static boolean userRegistered(long playerId) {
+        try (Jedis jedis = jedisPool.getResource()) {
+            System.out.println("userRegistered");
+            String userKey = USER_DB_MAP_KEY + ":" + playerId;
+            if (!jedis.exists(userKey)){
+                return false;
+            }
+            User checkedUser = convertJsonToUser(jedis.get(userKey));
+            return checkedUser.isRegistered();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("False");
+            return false;
+        }
+    }
+
+    static boolean userDeposited(long playerId) {
+        try (Jedis jedis = jedisPool.getResource()) {
+            System.out.println("userDeposited");
+            String userKey = USER_DB_MAP_KEY + ":" + playerId;
+            if (!jedis.exists(userKey)){
+                return false;
+            }
+            User checkedUser = convertJsonToUser(jedis.get(userKey));
+            return checkedUser.isDeposited();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("False");
+            return false;
+        }
+    }
+
+    static void registrationApprove(long playerId){
+        try (Jedis jedis = jedisPool.getResource()) {
+            System.out.println("userDeposited");
+            String userKey = USER_DB_MAP_KEY + ":" + playerId;
+            User checkedUser = convertJsonToUser(jedis.get(userKey));
+            checkedUser.setRegistered(true);
+            String updatedUser = convertUserToJson(checkedUser);
+            jedis.set(userKey, updatedUser);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void depositApprove(long playerId){
+        try (Jedis jedis = jedisPool.getResource()) {
+
+            String userKey = USER_DB_MAP_KEY + ":" + playerId;
+            User checkedUser = convertJsonToUser(jedis.get(userKey));
+            checkedUser.setDeposited(true);
+            String updatedUser = convertUserToJson(checkedUser);
+            jedis.set(userKey, updatedUser);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static String convertUserToJson(User user) {
+        Gson gson = new Gson();
+        return gson.toJson(user);
+    }
+
+    static User convertJsonToUser(String json) {
+        Gson gson = new Gson();
+        return gson.fromJson(json, User.class);
     }
 }
