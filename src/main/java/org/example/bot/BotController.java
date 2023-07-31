d1dde621453453ff9f3aaee9453554fe481f70c2
kostiantyn <sinkkostia@gmail.com>
2023-07-31 18:50:31 +0300

    bit testing 19

--- src/main/java/org/example/bot/BotController.java
+++ src/main/java/org/example/bot/BotController.java
@@ -331,7 +331,7 @@ public class BotController {
                                 String userKey = USER_DB_MAP_KEY + ":" + playerId;
                                 User checkedUser = convertJsonToUser(jedis.get(userKey));
                                 String sendAdminUID = checkedUser.getUID();
-                                bot.execute(new SendMessage(Long.valueOf(AdminID), "User with Telegram ID<code>" + playerId + "</code> and UID <code>" + sendAdminUID + "</code> deposited. Write 'Y11111111' (telegram id) to approve and 'N1111111' to disapprove").parseMode(HTML));
+                                bot.execute(new SendMessage(Long.valueOf(AdminID), "User with Telegram ID<code>" + playerId + "</code> and UID <code>" + sendAdminUID + "</code> \uD83D\uDFE1\uD83D\uDFE8 deposited. Write 'Y11111111' (telegram id) to approve and 'N1111111' to disapprove").parseMode(HTML));
                             } catch (Exception e) {
                                 bot.execute(new SendMessage(playerId, "❌ An error occurred. Please try again. "));
                                 e.printStackTrace();
@@ -406,7 +406,7 @@ public class BotController {
                                 User user = convertJsonToUser(jedis.get(userKey));
                                 String sendAdminUID = user.getUID();
                                 if (Integer.parseInt(sendAdminUID.substring(0, 1)) >= firstDigit && Integer.parseInt(sendAdminUID.substring(1, 2)) >= secondDigit) {
-                                    bot.execute(new SendMessage(Long.valueOf(AdminID), "User with Telegram ID<code>" + playerId + "</code> and UID <code>" + sendAdminUID + "</code> want to register. Write 'A11111111' (telegram id) to approve and 'D1111111' to disapprove").parseMode(HTML));
+                                    bot.execute(new SendMessage(Long.valueOf(AdminID), "User with Telegram ID<code>" + playerId + "</code> and UID <code>" + sendAdminUID + "</code> \uD83D\uDFE2\uD83D\uDFE9 want to register. Write 'A11111111' (telegram id) to approve and 'D1111111' to disapprove").parseMode(HTML));
                                     bot.execute(new SendMessage(playerId, "⏳ Great, your UID will be verified soon"));
                                 } else {
                                     InlineKeyboardButton button12 = new InlineKeyboardButton("Register here");
