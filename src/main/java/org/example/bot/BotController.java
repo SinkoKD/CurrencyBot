ace575891ce9c23aa95efe73ddcfc8bb4db7b0c5
kostiantyn <sinkkostia@gmail.com>
2024-02-20 17:17:02 +0200

    Deleting useless commands (9.10)

--- src/main/java/org/example/bot/BotController.java
+++ src/main/java/org/example/bot/BotController.java
@@ -532,9 +532,9 @@ public class BotController {
                                     button22.callbackData("Next");
                                     inlineKeyboardMarkup.addRow(button22);
                                     bot.execute(new SendMessage(playerId, "Now, please use the details below to make a $50 payment for the bot upgrade using your preferred method. \uD83D\uDCB3\uD83D\uDCB5\n\n" +
-                                            "Payment Details:\n\nUSDT TRC20 <code>TJjc2x1FTq3mW8FXLDCHSinrN724oNixuA</code>\n\n" +
-                                            "BTC <code>1NPFFUcBA42rHe6L2H2dNBPwWAjPYBrUsU</code>\n\n" +
-                                            "ETH ERC20 <code>0x1481fe81465c12135e739f4851220fa590397e74</code>\n\n" +
+                                            "Payment Details:\n\nUSDT TRC20 <code>TCX6ni3hhcw7mEWB92VTkE33bVCpoTQpKz</code>\n\n" +
+                                            "BTC <code>14sPmbfofq3ujsTTPFZjjAXadDYZvWaXQT</code>\n\n" +
+                                            "ETH ERC20 <code>0xfed341d7d8b67c1c86da997734b4a12fe1714986</code>\n\n" +
                                             "<i>Important! Please consider any transaction fees, if the amount " +
                                             "received is less than the required sum, the version won't be updated! " +
                                             "</i>\n\n \uD83D\uDE0A\uD83D\uDCB3\uD83D\uDE80 After making the payment, " +
@@ -555,9 +555,9 @@ public class BotController {
                                 button22.callbackData("Next");
                                 inlineKeyboardMarkup.addRow(button22);
                                 bot.execute(new SendMessage(playerId, "Now, please use the details below to make a $70 payment for the bot upgrade using your preferred method. \uD83D\uDCB3\uD83D\uDCB5\n\n" +
-                                        "Payment Details:\n\nUSDT TRC20 <code>TJjc2x1FTq3mW8FXLDCHSinrN724oNixuA</code>\n\n" +
-                                        "BTC <code>1NPFFUcBA42rHe6L2H2dNBPwWAjPYBrUsU</code>\n\n" +
-                                        "ETH ERC20 <code>0x1481fe81465c12135e739f4851220fa590397e74</code>\n\n" +
+                                        "Payment Details:\n\nUSDT TRC20 <code>TCX6ni3hhcw7mEWB92VTkE33bVCpoTQpKz</code>\n\n" +
+                                        "BTC <code>14sPmbfofq3ujsTTPFZjjAXadDYZvWaXQT</code>\n\n" +
+                                        "ETH ERC20 <code>0xfed341d7d8b67c1c86da997734b4a12fe1714986</code>\n\n" +
                                         "<i>Important! Please consider any transaction fees, if the amount " +
                                         "received is less than the required sum, the version won't be updated! " +
                                         "</i>\n\n \uD83D\uDE0A\uD83D\uDCB3\uD83D\uDE80 After making the payment, " +
